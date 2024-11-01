8e70e45c3c34c2951b491aa24c7bd087fc9a2c86
RoyGeova07 <royumana3@gmail.com>
2024-11-01 16:50:52 -0600

    TERMINADO

---
+++ src/main/JuegoAhorcado.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package main;
+
+/**
+ *
+ * @author royum
+ */
+public interface JuegoAhorcado {
+    
+    public void InicializarPalabraSecreta();
+    
+    public void jugar();
+    
+}
